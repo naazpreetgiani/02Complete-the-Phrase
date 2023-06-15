b4e71b93e490f742f132178798ca5c72cfd1eaae
naazpreetgiani <124805700+naazpreetgiani@users.noreply.github.com>
2023-06-15 09:20:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//Complete the Phrase Exercise
+
+//Add an Event Listener to the button to run the BLANK function
+document.getElementById("btn1").addEventListener("click", btn1);
+
+function btn1() {
+  let msg = "devil.";
+  alert(msg);
+}
+
+document.getElementById("btn2").addEventListener("click", btn2);
+
+function btn2() {
+  let msg = "short.";
+  alert(msg);
+}
+
+document.getElementById("btn3").addEventListener("click", btn3);
+
+function btn3() {
+  let msg = "together.";
+  alert(msg);
+}
